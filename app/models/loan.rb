cfb7873380b9af5ddaeaad116d7e3586889c959b
NessEFC <NessEFC@gmail.com>
2017-09-27 17:37:50 -0400

    add outstanding balance to loans, add default loan balance callback

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,3 +1,12 @@
 class Loan < ActiveRecord::Base
+  before_create :set_default_outstanding_balance
+
   has_many :payments
+
+  private
+
+    def set_default_outstanding_balance
+      self.outstanding_balance = self.funded_amount
+    end
+
 end
