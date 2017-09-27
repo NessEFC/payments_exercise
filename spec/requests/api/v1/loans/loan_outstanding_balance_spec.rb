82cd76e8050d979d667cd4761584bfb28f444d0c
NessEFC <NessEFC@gmail.com>
2017-09-27 18:19:47 -0400

    add test for displaying all outstanding balances on loans index

--- spec/requests/api/v1/loans/loan_outstanding_balance_spec.rb
+++ spec/requests/api/v1/loans/loan_outstanding_balance_spec.rb
@@ -25,4 +25,40 @@ RSpec.describe 'Show the outstanding balance', type: :request do
       assert(loan_response['updated_at'])
     end
   end
+
+  context 'for all loans' do
+    it 'displays all balances' do
+      loan_1, loan_2 = create_list(:loan, 2)
+      payment_1 = 100.00
+      payment_2 = 200.00
+      payment_3 = 500.00
+
+      post "/api/v1/loans/#{loan_1.id}/payments", { amount: payment_1 }
+      post "/api/v1/loans/#{loan_1.id}/payments", { amount: payment_2 }
+      post "/api/v1/loans/#{loan_2.id}/payments", { amount: payment_3 }
+
+      outstanding_balance_1 = loan_1.funded_amount - payment_1 - payment_2
+      outstanding_balance_2 = loan_2.funded_amount - payment_3
+
+      get "/api/v1/loans"
+
+      loans = JSON.parse(response.body)
+
+      assert_response :success
+      expect(response).to be_success
+      expect(loans.count).to eq(2)
+
+      expect(loans.first['id']).to eq(1)
+      expect(loans.first['funded_amount']).to eq(ApplicationController.helpers.number_to_currency(loan_1.funded_amount))
+      expect(loans.first['outstanding_balance']).to eq(ApplicationController.helpers.number_to_currency(outstanding_balance_1))
+      assert(loans.first['created_at'])
+      assert(loans.first['updated_at'])
+
+      expect(loans.last['id']).to eq(2)
+      expect(loans.last['funded_amount']).to eq(ApplicationController.helpers.number_to_currency(loan_2.funded_amount))
+      expect(loans.last['outstanding_balance']).to eq(ApplicationController.helpers.number_to_currency(outstanding_balance_2))
+      assert(loans.last['created_at'])
+      assert(loans.last['updated_at'])
+    end
+  end
 end
